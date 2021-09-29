d002fd2eb8faf5593ac50933aa85f31414137d5b
kyoichi sugahara <81.s.kyo.19@gmail.com>
2021-09-30 03:22:42 +0900

    merge change for jpntaxi

--- planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.py
+++ planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.py
@@ -82,6 +82,33 @@ def generate_launch_description():
     with open(lane_following_param_path, 'r') as f:
         lane_following_param = yaml.safe_load(f)['/**']['ros__parameters']
 
+    pull_over_param_path = os.path.join(
+        get_package_share_directory('planning_launch'),
+        'config',
+        'scenario_planning',
+        'lane_driving',
+        'behavior_planning',
+        'behavior_path_planner',
+        'pull_over',
+        'pull_over.param.yaml',
+    )
+    with open(pull_over_param_path, 'r') as f:
+        pull_over_param = yaml.safe_load(f)['/**']['ros__parameters']
+
+    pull_out_param_path = os.path.join(
+        get_package_share_directory('planning_launch'),
+        'config',
+        'scenario_planning',
+        'lane_driving',
+        'behavior_planning',
+        'behavior_path_planner',
+        'pull_out',
+        'pull_out.param.yaml',
+    )
+    with open(pull_out_param_path, 'r') as f:
+        pull_out_param = yaml.safe_load(f)['/**']['ros__parameters']
+
+
     behavior_path_planner_param_path = os.path.join(
         get_package_share_directory('planning_launch'),
         'config',
@@ -93,6 +120,7 @@ def generate_launch_description():
     )
     with open(behavior_path_planner_param_path, 'r') as f:
         behavior_path_planner_param = yaml.safe_load(f)['/**']['ros__parameters']
+
 
     behavior_path_planner_component = ComposableNode(
         package='behavior_path_planner',
@@ -127,11 +155,13 @@ def generate_launch_description():
             avoidance_param,
             lane_change_param,
             lane_following_param,
+            pull_over_param,
+            pull_out_param,
             behavior_path_planner_param,
             {
                 'bt_tree_config_path':
                 [FindPackageShare('behavior_path_planner'),
-                 '/config/behavior_path_planner_tree.xml']
+                 '/config/behavior_path_planner_tree_pull_out_.xml']
             }
         ],
         extra_arguments=[
