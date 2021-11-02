0d9598b19ceb6155166f058e20d21cc72eeb6ba3
Kenji Miyake <kenji.miyake@tier4.jp>
2021-11-02 23:01:44 +0900

    Remove unused import
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.py
+++ planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.py
@@ -17,7 +17,6 @@ import os
 from ament_index_python.packages import get_package_share_directory
 import launch
 from launch.actions import DeclareLaunchArgument
-from launch.actions import ExecuteProcess
 from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
