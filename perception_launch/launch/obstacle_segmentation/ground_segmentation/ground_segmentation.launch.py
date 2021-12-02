ee8a0da82434adaff4741e0cbad6039d17f298fc
taichiH <azumade.30@gmail.com>
2021-12-02 15:58:12 +0900

    fix import

--- perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
+++ perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
@@ -20,12 +20,12 @@ from launch.actions import OpaqueFunction
 from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
+from launch_ros.substitutions import FindPackageShare
 import yaml
 
 
