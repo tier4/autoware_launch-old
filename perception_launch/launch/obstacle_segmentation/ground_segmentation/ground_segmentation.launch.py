e35bb13dc49b16b0eb50e37da4fd559cab7cd1da
satoshi-ota <satoshi.ota928@gmail.com>
2021-12-02 13:46:30 +0900

    rename filter package

--- perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
+++ perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
@@ -300,8 +300,8 @@ def launch_setup(context, *args, **kwargs):
     )
 
     occupancy_outlier_filter_component = ComposableNode(
-        package="outlier_filter",
-        plugin="outlier_filter::OccupancyGridMapOutlierFilterComponent",
+        package="occupancy_grid_map_outlier_filter",
+        plugin="occupancy_grid_map_outlier_filter::OccupancyGridMapOutlierFilterComponent",
         name="occupancy_grid_map_outlier_filter",
         remappings=[
             ("~/input/occupancy_grid_map", "/perception/occupancy_grid_map/map"),
