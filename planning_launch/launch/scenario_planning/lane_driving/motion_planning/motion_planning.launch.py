19cd58d65856ea6e88008201505d8f10f55bcc29
Takayuki Murooka <takayuki5168@gmail.com>
2022-03-31 16:20:18 +0900

    update plugin name
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning_launch/launch/scenario_planning/lane_driving/motion_planning/motion_planning.launch.py
+++ planning_launch/launch/scenario_planning/lane_driving/motion_planning/motion_planning.launch.py
@@ -131,15 +131,54 @@ def generate_launch_description():
         obstacle_velocity_planner_param = yaml.safe_load(f)['/**']['ros__parameters']
     obstacle_velocity_planner_component = ComposableNode(
         package='obstacle_velocity_planner',
-        plugin='ObstacleVelocityPlanner',
+        plugin='motion_planning::ObstacleVelocityPlannerNode',
         name='obstacle_velocity_planner',
         namespace='',
         remappings=[
+            ("~/input/trajectory", "/planning/scenario_planning/lane_driving/motion_planning/surround_obstacle_checker/trajectory"),
+            ("~/input/odometry", "/localization/kinematic_state"),
+            ("~/input/map", "/map/vector_map"),
+            ("~/input/objects", "/perception/object_recognition/objects"),
+
             ("~/output/trajectory", "/planning/scenario_planning/lane_driving/trajectory"),
-            ('~/input/trajectory', 'surround_obstacle_checker/trajectory'),
+            ("~/output/velocity_limit", "/planning/scenario_planning/max_velocity_candidates"),
+            ("~/output/clear_velocity_limit", "/planning/scenario_planning/clear_velocity_limit"),
+            ("~/output/stop_reason", "/planning/scenario_planning/status/stop_reason"),
+            ("~/output/stop_reasons", "/planning/scenario_planning/status/stop_reasons"),
         ],
         parameters=[
             obstacle_velocity_planner_param,
+        ],
+        extra_arguments=[
+            {'use_intra_process_comms': LaunchConfiguration('use_intra_process')}
+        ],
+    )
+
+    # adaptive cruise controller
+    adaptive_cruise_controller_param_path = os.path.join(
+        get_package_share_directory('adaptive_cruise_controller'),
+        'config',
+        'adaptive_cruise_control.param.yaml',
+    )
+    with open(adaptive_cruise_controller_param_path, 'r') as f:
+        adaptive_cruise_controller_param = yaml.safe_load(f)['/**']['ros__parameters']
+    adaptive_cruise_controller_component = ComposableNode(
+        package='adaptive_cruise_controller',
+        plugin='motion_planning::AdaptiveCruiseControllerNode',
+        name='adaptive_cruise_controller',
+        namespace='',
+        remappings=[
+            ("~/input/trajectory", "/planning/scenario_planning/lane_driving/motion_planning/surround_obstacle_checker/trajectory"),
+            ("~/input/odometry", "/localization/kinematic_state"),
+            ("~/input/map", "/map/vector_map"),
+            ("~/input/objects", "/perception/object_recognition/objects"),
+
+            ("~/output/trajectory", "/planning/scenario_planning/lane_driving/trajectory"),
+            ("~/output/velocity_limit", "/planning/scenario_planning/max_velocity_candidates"),
+            ("~/output/clear_velocity_limit", "/planning/scenario_planning/clear_velocity_limit"),
+        ],
+        parameters=[
+            adaptive_cruise_controller_param,
         ],
         extra_arguments=[
             {'use_intra_process_comms': LaunchConfiguration('use_intra_process')}
@@ -208,6 +247,7 @@ def generate_launch_description():
         composable_node_descriptions=[
             obstacle_avoidance_planner_component,
             obstacle_velocity_planner_component,
+            # adaptive_cruise_controller_component,
             # obstacle_stop_planner_component,
         ],
     )
