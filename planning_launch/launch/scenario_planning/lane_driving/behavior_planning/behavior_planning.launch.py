be02d70a412ba73b9ea0b79b8d3f8fc920d634ea
Takayuki Murooka <takayuki5168@gmail.com>
2022-12-07 18:57:58 +0900

    update common and behavior param
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.py
+++ planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.py
@@ -173,7 +173,7 @@ def generate_launch_description():
             {
                 "bt_tree_config_path": [
                     FindPackageShare("behavior_path_planner"),
-                    "/config/behavior_path_planner_tree.xml",
+                    "/config/behavior_path_planner_tree_lane_change_only.xml",
                 ],
                 "planning_hz": 10.0,
             },
